5cbb6c741887f417cba88336d36aed8a945f6def
M1m1c <M1m1c@users.noreply.github.com>
2023-01-10 17:26:26 +0100

    continuing work on input system

--- Assets/Scripts/PlayerInputSystem.cs
+++ Assets/Scripts/PlayerInputSystem.cs
@@ -9,23 +9,61 @@ using UnityEngine.InputSystem;
 
 public struct InputComp : IComponentData
 {
-    public BitField32 value;
+    public bool[] value;
 }
 
-public partial class PlayerInputSystem : SystemBase
+public partial struct PlayerInputSystem : ISystem
 {
     private InputActions m_InputActions;
+    private InputActions.PlayerActionMapActions m_PlayerInputActions;
 
-    protected override void OnCreate()
+    public void OnDestroy(ref SystemState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnCreate(ref SystemState state)
     {
         m_InputActions = new InputActions();
         m_InputActions.Enable();
+
+        m_PlayerInputActions = m_InputActions.PlayerActionMap;
     }
 
-    protected override void OnUpdate()
+    public void OnUpdate(ref SystemState state)
     {
         InputSystem.Update();
 
+        bool[] value = new bool[5];
+
+        //TODO do this for each action and toggle the inputs
       
+        value[0] = m_PlayerInputActions.MoveForward.ReadValue<bool>();
+
+
+        EntityQuery query = new EntityQueryBuilder(Allocator.Temp)
+            .WithAllRW<InputComp>()
+            .WithAll<Client>()
+            .Build(ref state);
+
+        var entities = query.ToEntityArray(Allocator.Temp);
+       // var inputComps = state.GetComponentLookup<InputComp>(false);
+
+        //TODO should probably only apply to local player
+        //foreach (var entity in entities)
+        //{
+        //   var inputComp = inputComps[entity];
+        //    //TODO Set inputs
+        //}
+    }
+
+    void ISystem.OnCreate(ref SystemState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void ISystem.OnUpdate(ref SystemState state)
+    {
+        throw new System.NotImplementedException();
     }
 }
