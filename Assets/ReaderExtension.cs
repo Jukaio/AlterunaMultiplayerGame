8bf96145580ca22089348450e745781ba97ac379
David <d.naussed@outlook.de>
2023-01-12 10:44:21 +0100

    * Writer/Reader extensions

---
+++ Assets/ReaderExtension.cs
@@ -0,0 +1,13 @@
+﻿using Alteruna;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+public static class ReaderExtension
+{
+    public static NativeArray<T> Read<T>(this Reader reader) where T : unmanaged
+    {
+        var asBytes = reader.ReadByteArray();
+        var bytes = new NativeArray<byte>(asBytes, Allocator.Temp);
+        return bytes.Reinterpret<T>(UnsafeUtility.SizeOf<T>());
+    }
+}
---
+++ Assets/WriterExtension.cs
@@ -0,0 +1,14 @@
+﻿using Alteruna;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+public static class WriterExtension
+{
+    public static void Write<T>(this Writer writer, NativeArray<T> array) where T : unmanaged
+    {
+        var asBytes = array.Reinterpret<byte>(UnsafeUtility.SizeOf<byte>());
+
+        // Maybe reinterpret NativeArray directly as C# array with offset of members
+        writer.Write(asBytes.ToArray());
+    }
+}
