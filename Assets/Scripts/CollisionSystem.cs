a276a3b8f5a14ae4de7a39f53bad02674c535e4b
M1m1c <M1m1c@users.noreply.github.com>
2023-01-13 11:55:57 +0100

    Created CollisionSystem Script

---
+++ Assets/Scripts/CollisionSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+using Unity.Mathematics;
+
+public partial struct CollisionSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        throw new System.NotImplementedException();
+    }
+}
