bad572211e32ce31dda9d7f39aea64447763c485
M1m1c <M1m1c@users.noreply.github.com>
2023-01-17 10:34:52 +0100

    created EntitySpawner script

---
+++ Assets/Scripts/EntitySpawner.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using UnityEngine.InputSystem;
+using Unity.Mathematics;
+
+public class EntitySpawner : MonoBehaviour, IComponentData
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        _ = manager.CreateSingleton(this, "Entity Spawner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TODO check if this needs to be called on each instance via RPC
+    public Entity SpawnEntity(uint syncID, ref SystemState state, EntityArchetype archetype)
+    {
+        var manager = state.EntityManager;
+
+        var entity = manager.CreateEntity(archetype);
+        manager.AddComponent(entity, typeof(SyncID));
+        manager.SetComponentData(entity, new SyncID { value = syncID });
+   
+        var query = manager.CreateEntityQuery(typeof(UserToEntityTranslator));
+
+        var arr = query.ToComponentArray<IDToEntityTranslator>();
+        if (arr.Length != 0) 
+        {
+            var translator = arr[0];
+            translator.IDEntityMap.Add(syncID, entity);
+        }
+       
+        return entity;
+    }
+
+    //TODO this should remove entity with id from local IDEntityMap
+    public void DestroyEntity()
+    {
+        //var arr = query.ToComponentArray<IDToEntityTranslator>();
+        //if (arr.Length != 0)
+        //{
+        //    var translator = arr[0];
+        //    translator.IDEntityMap.Remove(syncID);
+        //}
+    }
+}
