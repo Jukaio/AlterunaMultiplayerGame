d6e6c731160b9d101ae44a114abc11d05c84fa95
M1m1c <M1m1c@users.noreply.github.com>
2023-01-18 11:50:55 +0100

    Reverted syncID usage and left reminder comment

--- Assets/PlayerClient.cs
+++ Assets/PlayerClient.cs
@@ -54,24 +54,21 @@ public class PlayerClient : MonoBehaviour
             typeof(SizeComp),
             typeof(ColliderComp));
 
+        //TODO so we have and issue with host and client ID managers not being synced.
+        // when we join our ID manager looks like the hosts manager.
+        // However on the hosts side when the joiner entity is added as a remote the ID manager has not yet been synced from the joiners game instance.
+        // Meanign the remote on the hosts side will try to get the hosts last syncID  whihc is the one the host local player is already using.
+        // likewise another problem occurs on the joining players world, the local player gets created first gaining a new syncID,
+        // however now when it tries to ccreate the remotes it will not use the syncID of the host, it will use the last requested which is the local users index.
+        // A probable solution for this would be to not have players use sync IDs and let them only use user indexes.
+        // WHile for spawning projectiles and stuff we would have to do RPC calls for spawning where we forward the syncID of the spawned entity.
 
-        var spawnQuery = manager.CreateEntityQuery(typeof(EntitySpawner));
-        var spawner = spawnQuery.GetSingleton<EntitySpawner>();
+        //Antoher problem that we have is that the order of instantiation is weird.
+        // If we are joining our player entity gets spawend first, then all remotes.
+        // If we are hosting remotes get spawned before a new ID has been requested and synced
+   
 
-        var idQuery = manager.CreateEntityQuery(typeof(IDManager));
-        var idManager = idQuery.GetSingleton<IDManager>();
-        uint syncID;
-
-        if (avatar.IsMe)
-        {
-            syncID = idManager.RequestID();
-        }
-        else
-        {
-            syncID = idManager.GetLastRequestedID();
-        }
-
-        var e = spawner.SpawnEntity(syncID, manager, clientArchetype); //manager.CreateEntity(clientArchetype);
+        var e = manager.CreateEntity(clientArchetype);
         manager.SetComponentData(e, new Player { index = user.Index });
         manager.SetComponentData(e, new Velocity { value = new Unity.Mathematics.float3(0.0f, 0.0f, 0.0f) });
         manager.SetComponentData(e, new Rotation { value = 0.0f });
