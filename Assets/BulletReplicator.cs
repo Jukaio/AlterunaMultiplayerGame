a5a0312d9fc77823903029bc87ca56ca60e3c1bc
M1m1c <M1m1c@users.noreply.github.com>
2023-01-20 17:36:43 +0100

    added team to bullet replication

--- Assets/BulletReplicator.cs
+++ Assets/BulletReplicator.cs
@@ -8,23 +8,27 @@ public class BulletReplicator : Replicator
     public override void OnAssembleData(in EntityQuery query, Writer writer, byte LOD = 100)
     {
         writer.Write(query.ToComponentDataArray<Position>(Allocator.Temp));
+        writer.Write(query.ToComponentDataArray<Team>(Allocator.Temp));
     }
 
     public override void OnCreate(out ComponentType[] types)
     {
-        types = new ComponentType[2];
+        types = new ComponentType[3];
         types[0] = typeof(Bullet);
         types[1] = typeof(Position);
+        types[2] = typeof(Team);
     }
 
     public override void OnDisassembleData(in UnsafeList<Entity> entities, Reader reader, byte LOD = 100)
     {
         var manager = World.EntityManager;
         var positions = reader.ReadArray<Position>();
+        var teams = reader.ReadArray<Team>();
 
         for (int i = 0; i < entities.Length; i++) {
             var entity = entities[i];
             manager.SetComponentData(entity, positions[i]);
+            manager.SetComponentData(entity, teams[i]);
         }
     }
 }
