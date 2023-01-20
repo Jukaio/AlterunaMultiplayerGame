2b74ca542a29cac87249c5aba52968a8a845287d
M1m1c <M1m1c@users.noreply.github.com>
2023-01-20 17:33:08 +0100

    set bullet materials in sprite lib

--- Assets/SpriteLibrary.cs
+++ Assets/SpriteLibrary.cs
@@ -20,11 +20,19 @@ public class SpriteLibrary : MonoBehaviour, IComponentData
     [SerializeField]
     private Material team2Material;
 
+    [SerializeField]
+    private Material team1BulletMaterial;
+
+    [SerializeField]
+    private Material team2BulletMaterial;
+
     public Mesh Mesh => quadMesh;
     public Material DefaultMaterial => defaultMaterial;
     public Material BulletMaterial => bulletMaterial;
     public Material Team1Material => team1Material;
     public Material Team2Material => team2Material;
+    public Material Team1BulletMaterial => team1BulletMaterial;
+    public Material Team2BulletMaterial => team2BulletMaterial;
 
 
     void Start()
