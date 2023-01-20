83e359a09d552435f25c80ebb55b750c38cc59b8
M1m1c <M1m1c@users.noreply.github.com>
2023-01-20 15:56:23 +0100

    set new materials in sprite lib

--- Assets/SpriteLibrary.cs
+++ Assets/SpriteLibrary.cs
@@ -14,9 +14,17 @@ public class SpriteLibrary : MonoBehaviour, IComponentData
     [SerializeField]
     private Material bulletMaterial;
 
+    [SerializeField]
+    private Material team1Material;
+
+    [SerializeField]
+    private Material team2Material;
+
     public Mesh Mesh => quadMesh;
     public Material DefaultMaterial => defaultMaterial;
     public Material BulletMaterial => bulletMaterial;
+    public Material Team1Material => team1Material;
+    public Material Team2Material => team2Material;
 
 
     void Start()
