9e392b2d67906c3203d8f23269bbdfa9715071e7
M1m1c <M1m1c@users.noreply.github.com>
2023-01-23 13:04:04 +0100

    finished screen wrap system

--- Assets/Scripts/ScreenWrapSystem.cs
+++ Assets/Scripts/ScreenWrapSystem.cs
@@ -1,20 +1,61 @@
-using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Entities;
+using UnityEngine;
+using Unity.Mathematics;
 
 public partial struct ScreenWrapSystem : ISystem
 {
+    EntityQuery query;
+    ComponentLookup<Position> positions;
+
     public void OnCreate(ref SystemState state)
     {
-        throw new System.NotImplementedException();
+        query = new EntityQueryBuilder(Allocator.Temp)
+            .WithAllRW<Position>()
+            .WithAll<Player, Local>()
+            .Build(ref state);
+        positions = state.GetComponentLookup<Position>(false);
     }
 
     public void OnDestroy(ref SystemState state)
     {
-        throw new System.NotImplementedException();
+      
     }
 
     public void OnUpdate(ref SystemState state)
     {
-        throw new System.NotImplementedException();
+        var manager = state.EntityManager;
+
+        var entities = query.ToEntityArray(Allocator.Temp);
+        positions.Update(ref state);
+
+        foreach (var entity in entities)
+        {
+            var currentPos = positions[entity].value;
+            float3 posToSet = currentPos;
+
+            if(currentPos.x > 7.5f)
+            {
+                posToSet.x = -7f;
+            }
+            else if(currentPos.x < -7.5f)
+            {
+                posToSet.x = 7f;
+            }
+
+            if (currentPos.y > 6.6f)
+            {
+                posToSet.y = -4.5f;
+            }
+            else if (currentPos.y < -4.7f)
+            {
+                posToSet.y = 6.4f;
+            }
+
+            positions[entity] = new Position { value = posToSet };
+
+        }
     }
 }
