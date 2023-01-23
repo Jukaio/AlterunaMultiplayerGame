2f7604a8c0bd81268d9711ddef9108ea06264680
M1m1c <M1m1c@users.noreply.github.com>
2023-01-23 10:32:17 +0100

    created ScreenWrapSystem script

---
+++ Assets/Scripts/ScreenWrapSystem.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Unity.Entities;
+
+public partial struct ScreenWrapSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        throw new System.NotImplementedException();
+    }
+}
